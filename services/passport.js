86e9ab8586b09706c2dc932aeba886936a9e2383
redouane <redouane.hitachi@gmail.com>
2024-11-10 05:25:48 -0800

    Authentication with passport&cookie&googleAuth

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,35 @@
 
 const passport = require('passport');
 const GoogleStartegy = require('passport-google-oauth20');
-const keys = require('../config/Keys')
+const keys = require('../config/Keys');
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+});
+
 
 passport.use(new GoogleStartegy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile token', profile);
-    console.log('done token', done);
+    User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+            done(null, existingUser);
+        } else {
+            new User({ googleId: profile.id }).save().then((user) => {
+                done(null, user)
+            })
+        }
+    })
+
 })
 );
